6f9cf815ddbc54aed4fa0cef60d5584c4d17a186
hidingcode <yclim4635@gmail.com>
2023-04-19 14:12:35 +0800

    Forget password UI

--- app/src/main/java/com/example/harganow/presentation/user/ForgetPasswordScreen.kt
+++ app/src/main/java/com/example/harganow/presentation/user/ForgetPasswordScreen.kt
@@ -1,6 +1,7 @@
 package com.example.harganow.presentation.user
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
@@ -9,18 +10,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.harganow.R
 import com.example.harganow.ui.theme.Orange
 
 @Composable
-fun ResetPasswordScreen(
+fun ForgetPasswordScreen(
     navigateToHome: () -> Unit,
     navigateToForgetPassword: () -> Unit,
 ) {
-    var newPassword by remember { mutableStateOf("") }
-    var confirmPassword by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
     val context = LocalContext.current
 
     Column(
@@ -29,69 +31,63 @@ fun ResetPasswordScreen(
             .background(Color.White)
     ) {
 
-        Header(title = "Reset Password", titleSize = 64, navigateToHome = navigateToHome)
+        Header(title = "Forget Password", titleSize = 64, navigateToHome = navigateToHome)
         Column(
             modifier = Modifier
                 .padding(32.dp)
         ) {
-            Spacer(modifier = Modifier.height(16.dp))
 
-            OutlinedTextField(
-                value = newPassword,
-                onValueChange = {newPassword = it},
-                placeholder = { Text("New Password") },
+            Image(
+                painter = painterResource(id = R.drawable.forget_password_image),
+                contentDescription = "Forget Password Image",
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
-                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+                    .size(200.dp)
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
             OutlinedTextField(
-                value = confirmPassword,
-                onValueChange = {confirmPassword = it},
-                placeholder = { Text("Confirm Password") },
+                value = email,
+                onValueChange = {email = it},
+                placeholder = { Text("Your Email") },
                 modifier = Modifier
                     .align(Alignment.CenterHorizontally)
                     .fillMaxWidth()
             )
 
+            Spacer(modifier = Modifier.height(8.dp))
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "An verification email will be sent to you.",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Click the link to retrieve your password.",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
 
             Button(
                 onClick = {
-                    if (newPassword.isEmpty() || confirmPassword.isEmpty()){
+                    if (email.isEmpty()){
                         Toast.makeText(context, "Please fill all fields", Toast.LENGTH_SHORT)
                             .show()
                         return@Button
                     }
-
-                    if(confirmPassword == newPassword) {
-                        // TODO : Reset password
-                    } else {
-                        Toast.makeText(context, "Confirm password do not match your password", Toast.LENGTH_SHORT)
-                            .show()
-                        return@Button
-                    }
+                    // TODO: Sent email to user
                 },
                 modifier = Modifier
                     .fillMaxWidth(),
                 colors = ButtonDefaults.buttonColors(backgroundColor = Orange)
             ) {
                 Text(
-                    text = "RESET",
+                    text = "SEND",
                     color = Color.White
-                )
-            }
-            
-            TextButton(onClick = { navigateToForgetPassword() }) {
-                Text(
-                    text = "Forget Your Password?",
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    color = Orange
                 )
             }
         }
@@ -100,8 +96,8 @@ fun ResetPasswordScreen(
 
 @Preview
 @Composable
-fun ResetPasswordScreenPreview() {
-    ResetPasswordScreen(
+fun ForgetPasswordScreenPreview() {
+    ForgetPasswordScreen(
         navigateToHome = {},
         navigateToForgetPassword = {},
     )
